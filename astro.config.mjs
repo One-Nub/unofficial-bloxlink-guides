cf5b8d196102b85d8fae7ea977595d0563d3c372
Nub <38899321+One-Nub@users.noreply.github.com>
2025-07-16 05:53:00 +0000

    Add favicon
    Used realfavicongenerator.net

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,6 +34,48 @@ export default defineConfig({
         },
       ],
       lastUpdated: true,
+      favicon: "/public/favicon.svg",
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            type: "image/png",
+            href: "/favicon-96x96.png",
+            sizes: "96x96",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            type: "image/svg+xml",
+            href: "/favicon.svg",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "shortcut icon",
+            href: "/favicon.ico",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+            href: "/apple-touch-icon.png",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "manifest",
+            href: "/site.webmanifest",
+          },
+        },
+      ],
     }),
   ],
 });
